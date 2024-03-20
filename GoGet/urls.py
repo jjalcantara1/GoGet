81c2c235e59c395a2ec59eea01ccfed2f876389e
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2024-03-20 23:37:20 +0800

    booking

--- GoGet/urls.py
+++ GoGet/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- book/views.py
+++ book/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from adminpanel.models import Booking
 from .models import Order
 from .serializers import OrderSerializer
 from rest_framework import viewsets
 
+
 @api_view(['POST'])
 def CreateOrder(request):
         data = request.data
-        print(data['contact_no'])
+
+        try:
+                booking = Booking.objects.get(id=data['booking_id'])
+        except Booking.DoesNotExist:
+                return Response({'error': 'Booking not found'}, status=404)
+
         order = Order.objects.create(
-                name = data['name'],
-                email = data['email'],
-                contact_no= data['contact_no']
-                )
+                booking=booking,
+                name=data['name'],
+                email=data['email'],
+                contact_no=data['contact_no']
+        )
+
         serializer = OrderSerializer(order, many=False)
-        return Response({'message: success'})
+
+        return Response(serializer.data, status=201)
+
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
