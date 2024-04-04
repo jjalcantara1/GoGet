7cb3242eee2102c770fd3cb511abd6dd19bee6f8
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2024-04-04 23:05:09 +0800

    order summary

--- book/serializers.py
+++ book/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+
+from filters.serializers import BookingSerializer
 from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
+    bookings = BookingSerializer(many=True, read_only=True)
+
     class Meta:
         model = Order
         fields = '__all__'
